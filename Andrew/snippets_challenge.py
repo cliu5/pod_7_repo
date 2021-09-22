36019b1315924f39a548d4763069ecda8037367f
Andrew Pak <Andrew.Pak78@qmail.cuny.edu>
2021-09-21 23:02:43 -0400

    snippets challenge complete

--- Andrew/snippets_challenge.py
+++ Andrew/snippets_challenge.py
@@ -8,7 +8,7 @@ print("Code Snippet 1:")
 u = 5
 v = 2
 
-if u * v = 10:
+if u * v == 10:
     print(f"The product of u ({u}) and v ({v}) is 10")
 else:
     print(f"The product of u ({u}) and v ({v}) is not 10")
@@ -24,10 +24,10 @@ z = 30
 if z < x:
     print("z is less than x")
 
-elif z > x and z < y
+elif z > x and z < y:
     print("z is between x and y")
 
-else
+else:
     print("z is greater than y")
 
 
@@ -40,9 +40,9 @@ print("Code Snippet 3:")
 
 a = 1
 b = 1
-c = (a > b)
+c = (a >= b)
 
-print(f"The value of c ({c}) is True since a ({a}) is greater than b ({b}).")
+print(f"The value of c ({c}) is True since a ({a}) is greater or equal to b ({b}).")
 assert(c == True) #Do not change this line
 
 print()
@@ -51,8 +51,9 @@ print("Code Snippet 4:")
 
 #modify exactly one boolean operator in the assignment of d, so that d evaluates to False
 
-d = (5 < 7) or not (8 < 20)
+d = (5 > 7) or not (8 < 20)
 # TO DO: Explain how d is set to False in a print statement
+print(f"The value of d ({d}) is False because 5 is not greater than 7 and 8 is indeed less that 20 ")
 assert(d == False) #Do not change this line
 
 print()
@@ -66,7 +67,7 @@ print("Code Snippet 5:")
 m = "GOAT"
 n = "goat"
 
-o = (m == n)
+o = (m != n)
 
 print (f"The value of o ({o}) is True since Python is case-sensitive.")
 assert(o == True) #Do not change this line
