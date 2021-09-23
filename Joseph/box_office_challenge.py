b1c1a20a5bef01e0dfa40c694b588d4ad7cf367f
Joe Bollinger <joeboo865@gmail.com>
2021-09-22 20:37:26 -0400

    completed box office challenge

--- Joseph/box_office_challenge.py
+++ Joseph/box_office_challenge.py
@@ -16,14 +16,14 @@ print(len(remaining_50_list))
 remaining_50_list.remove("The Matrix")
 # Note: Make sure to double check the list you are removing from!
 print(remaining_50_list)
-print(len(remaining_50_list))
+# print(len(remaining_50_list))
 
 
 # TODO: We're down to 49 movies in "remaining_50_list". Let's add a movie to the list called "Anywhere But Here"
 remaining_50_list.append("Anywhere But Here")
 # Note: Print the variable "remaining_50_list" and see if the movie has been added!
 print(remaining_50_list)
-print(len(remaining_50_list))
+# print(len(remaining_50_list))
 
 
 # TODO: Let's make sure "remaining_50_list" has exactly 50 movie titles
@@ -33,13 +33,13 @@ print(len(remaining_50_list))
 top_100 = remaining_50_list + top_50_list
 # Note: Print your new variable to see the combined list of movies
 print(top_100)
-print(len(top_100))
+# print(len(top_100))
 
 # TODO: Let's print the first movie in our "top_100" list. Let's use the index lookup method to do this.
 print(top_100[0])
 
 # TODO: Let's print the top 10 movies in "top_100" as a list. Let's use the slice syntax to accomplish this!
-print(top_100[0:11])
+print(top_100[0:10])
 print(len(top_100[0:10]))
 
 # TODO: Let's convert the list "top_100" back into a string. Assign it to a variable called "top_100_str".
