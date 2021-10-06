3424bbc6d5818c331a45e9d416f8e770514166e8
tford <tony.ford@gmail.com>
2021-10-05 19:17:08 -0500

    playlist challenge

---
+++ Tony/importing.py
@@ -0,0 +1,30 @@
+# abbr = ['mon', 'wednes', 'fri']
+# to_do = ['morning - buy groceries', 'evening - meal prep']
+# schedule = []
+
+# def fix_my_week():
+#     for day in abbr:
+#         for task in to_do:
+#             schedule.append((day + "day ").capitalize() + task)
+
+# fix_my_week()
+# print(schedule)
+
+
+# print("Hello")
+# print()
+# print('hello'.upeer())
+
+# import os
+
+# print(os.getLogin())
+# print()
+# print(os.system('dir'))
+
+
+import numpy as np
+
+print(np.arange(10))
+
+
+
