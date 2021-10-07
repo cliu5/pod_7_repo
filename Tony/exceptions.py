2c35aff40e35daeddfa23124dae89757a9508e55
tford <tony.ford@gmail.com>
2021-10-06 20:25:39 -0500

    catchup and challenge for exceptions

---
+++ Tony/exceptions.py
@@ -0,0 +1,15 @@
+# try:
+#     x= 56
+#     print(x)
+# except NameError:
+#     print("Variable x is undefined")
+# except:
+#     print('Something went wrong')
+
+
+try:
+    print(x)
+except:
+    print('Something went wrong!')
+finally:
+    print('We reached the end!')
