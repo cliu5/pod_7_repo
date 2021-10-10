7791e0454710abd9b93b2f01b22cdefc5d0dc07a
Serena Killion <srk2181@columbia.edu>
2021-10-10 11:59:00 -0400

    remove empty files, only commited complete challenges

--- Kevin/first_python_challenge.py
+++ Kevin/first_python_challenge.py
@@ -7,15 +7,26 @@ box_2 = 'clothes'
 box_3 = 'plants'
 #declaring variable box_4 and intializing the value to kitchen stuff which is a string
 box_4 = 'kitchen stuff'
+print()
+
 print('2: Print the variables box_1, box_2, box_3, box_4')
+#I printed print commands for the string Box1 Then For Box 2 through Box 4
 print(box_1)
 print(box_2)
-print(box_3)
+print(f'third box prints out {box_3}')
 print(box_4)
+print()
+
 print('3.1: Declare a variable with the name "address", assign it any street name you like')
+#elm street declared as a street name
 address ='elm st'
+print()
+
 print('3.2: Print the address variable')
+#print command for the address variable
 print(address)
+print()
+
 print('4: Reassign variables box_2 and box_4 with some other text and print box_1, box_2, box_3, box_4 again')
 box_2='money'
 box_4='time'
@@ -23,9 +34,17 @@ print(box_1)
 print(box_2)
 print(box_3)
 print(box_4)
+print()
+
 print('5.1: The line of code below is commented out because it produces many SyntaxErrors. Fix the problem and turn the comment back into regular Python code')
+#fixed syntax errors
+
 completion_message = 'Completed the first Python challenge!'
+print()
+
+
 print('5.2: Turn the comment below back into regular Python code')
 print(completion_message)
+#print command with completion message should show assigened value
 
 
