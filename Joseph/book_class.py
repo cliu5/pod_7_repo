d44c29ea30723d34ef62293ffb3dd24ab9903d15
Joe Bollinger <joeboo865@gmail.com>
2021-10-11 21:25:26 -0400

    Completed book challenge

---
+++ Joseph/book_class.py
@@ -0,0 +1,20 @@
+class Booklist():
+    def __init__(self):
+        self.books = []
+
+    def add(self, title, author):
+        book = {}
+        book['title'] = title
+        book['author'] = author
+        self.books.append(book)
+
+    def count_books(self):
+        return len(self.books)
+
+    def remove_title(self, title):
+        for book in self.books:
+            if book['title'] == title:
+                self.books.remove(book)
+
+    def display_titles(self):
+        return sorted(self.books, key=lambda x: x['title'])
