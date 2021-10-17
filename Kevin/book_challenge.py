7d6c992c3a6415f4d2a986827bc693a7b3783fc2
Kevin <kevincummings0001@yahoo.com>
2021-10-16 21:32:56 -0400

    test push

--- Kevin/book_challenge.py
+++ Kevin/book_challenge.py
@@ -61,7 +61,7 @@ Define the count_books() method to get the number of books in an object of class
 
 Once you have finished the method, count the books in my_library and print out the result
 '''
-print('\nPart 3\n')
+print('\nPart .3\n')
 
 
 
