e4c4965c8875382c1c9d0fa9122ab677638a0d37
Ramsy Anton <anton.ra@hotmail.com>
2021-10-21 21:15:31 -0700

    Completed Stations Challenge

---
+++ Ramsy/station_planning.py
@@ -0,0 +1,14 @@
+from stations_challenge import *
+
+
+Everett_Station = SubwayStation(station_name = 'Everett Station', 
+                        location= "Roosevelt Street", 
+                        lines = ['510', '512', 'N Line'])
+
+U_District = SubwayStation(station_name='U District', 
+                        location='Capitol & 1st Ave', 
+                        lines = ['532', '535', '542'])
+
+Lynnwood = BusStation(station_name = 'Lynnwood', 
+                            location = 'W Mukilteo & East St', 
+                            routes = ['Rucker', 'Glenwood', 'Hewitt'])
