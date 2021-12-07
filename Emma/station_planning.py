ab9270d57d8c7ce1a4c7b32a1a6f82a1ac0cc1fd
Guillermo Barbosa <billandassociatesllc@Guillermos-iMac.local>
2021-12-07 18:12:09 -0500

    completed todoproject/station_planning

---
+++ Emma/station_planning.py
@@ -0,0 +1,35 @@
+from stations_challenge import *
+
+
+class Station_Planning:
+    def __init__(self, stations, bus, train, subway):
+        self.stations = stations
+        self.bus = bus
+        self.train = train
+        self.subway = subway
+
+    def __init__(self, locations, lines, routes):
+        self.locations = locations
+        self.lines = lines
+        self.routes = routes
+
+
+station_name = 'Springfield BLVD'
+location:   '6100 Springield BLVD'
+lines: ['22nd', '33rd']
+
+station_name = 'Old Towne'
+location: '808 Duke ST'
+routes: ('King ST', 'Prince ST')
+
+station_name = ('Tysons Corner')
+location: ('8100 Spring Hill RD')
+
+if stations is open():
+    print(open_stations == True)
+
+else:
+    if stations is close():
+        print(close_stations=False)
+print(open_stations)
+print(close_stations)
